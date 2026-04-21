6b3ad1b9aba6b1a360da18ec0bfce3f0beb6f0ce
Qarani-m <emqarani@gmail.com>
2026-04-21 13:45:10 +0300

    feat: implement Linux bridge driver using nsenter and add staging deployment workflow

--- internal/driver/bridge.go
+++ internal/driver/bridge.go
@@ -8,6 +8,16 @@ import (
 	"go.uber.org/zap"
 )
 
+// hostCmd runs an ip command in the host network namespace via nsenter.
+// Requires the container to be started with --pid=host and --privileged (or NET_ADMIN).
+func hostCmd(args ...string) *exec.Cmd {
+	// nsenter -t 1 -n -- <args...>
+	// -t 1  : target PID 1 (init), which always lives in the host namespace
+	// -n    : enter the network namespace of that PID
+	full := append([]string{"-t", "1", "-n", "--"}, args...)
+	return exec.Command("nsenter", full...)
+}
+
 type BridgeDriver interface {
 	CreateBridge(name string) error
 	DeleteBridge(name string) error
@@ -34,17 +44,13 @@ func (d *linuxBridgeDriver) CreateBridge(name string) error {
 		return nil
 	}
 
-	zap.L().Info("Creating Linux bridge", zap.String("bridge", name))
+	zap.L().Info("Creating Linux bridge on host ns", zap.String("bridge", name))
 
-	// ip link add name <name> type bridge
-	cmd := exec.Command("ip", "link", "add", "name", name, "type", "bridge")
-	if out, err := cmd.CombinedOutput(); err != nil {
+	if out, err := hostCmd("ip", "link", "add", "name", name, "type", "bridge").CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to create bridge %s: %w (output: %s)", name, err, string(out))
 	}
 
-	// ip link set dev <name> up
-	cmd = exec.Command("ip", "link", "set", "dev", name, "up")
-	if out, err := cmd.CombinedOutput(); err != nil {
+	if out, err := hostCmd("ip", "link", "set", "dev", name, "up").CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to set bridge %s up: %w (output: %s)", name, err, string(out))
 	}
 
@@ -52,18 +58,15 @@ func (d *linuxBridgeDriver) CreateBridge(name string) error {
 }
 
 func (d *linuxBridgeDriver) DeleteBridge(name string) error {
-	zap.L().Info("Deleting Linux bridge", zap.String("bridge", name))
-	cmd := exec.Command("ip", "link", "del", "name", name)
-	if out, err := cmd.CombinedOutput(); err != nil {
+	zap.L().Info("Deleting Linux bridge on host ns", zap.String("bridge", name))
+	if out, err := hostCmd("ip", "link", "del", name).CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to delete bridge %s: %w (output: %s)", name, err, string(out))
 	}
-
 	return nil
 }
 
 func (d *linuxBridgeDriver) Exists(name string) (bool, error) {
-	cmd := exec.Command("ip", "link", "show", name)
-	err := cmd.Run()
+	err := hostCmd("ip", "link", "show", name).Run()
 	if err != nil {
 		if _, ok := err.(*exec.ExitError); ok {
 			return false, nil
@@ -74,50 +77,36 @@ func (d *linuxBridgeDriver) Exists(name string) (bool, error) {
 }
 
 func (d *linuxBridgeDriver) AssignIP(name, cidr string) error {
-	zap.L().Info("Assigning IP to bridge", zap.String("bridge", name), zap.String("cidr", cidr))
-
-	// ip addr add <cidr> dev <name>
-	cmd := exec.Command("ip", "addr", "add", cidr, "dev", name)
-	if out, err := cmd.CombinedOutput(); err != nil {
-		// Ignore if the address is already assigned
+	zap.L().Info("Assigning IP to bridge on host ns", zap.String("bridge", name), zap.String("cidr", cidr))
+	if out, err := hostCmd("ip", "addr", "add", cidr, "dev", name).CombinedOutput(); err != nil {
 		if strings.Contains(string(out), "File exists") {
 			return nil
 		}
 		return fmt.Errorf("failed to assign IP to bridge %s: %w (output: %s)", name, err, string(out))
 	}
-
 	return nil
 }
 
 func (d *linuxBridgeDriver) BringUp(name string) error {
-	zap.L().Info("Bringing up interface", zap.String("device", name))
-	cmd := exec.Command("ip", "link", "set", "dev", name, "up")
-	if out, err := cmd.CombinedOutput(); err != nil {
+	zap.L().Info("Bringing up interface on host ns", zap.String("device", name))
+	if out, err := hostCmd("ip", "link", "set", "dev", name, "up").CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to set interface %s up: %w (output: %s)", name, err, string(out))
 	}
 	return nil
 }
 
 func (d *linuxBridgeDriver) AddIPAlias(ip, device string) error {
-	zap.L().Info("Adding IP alias to device", zap.String("ip", ip), zap.String("device", device))
-
-	// ip addr add <ip>/32 dev <device>
-	cmd := exec.Command("ip", "addr", "add", ip+"/32", "dev", device)
-	if out, err := cmd.CombinedOutput(); err != nil {
+	zap.L().Info("Adding IP alias on host ns", zap.String("ip", ip), zap.String("device", device))
+	if out, err := hostCmd("ip", "addr", "add", ip+"/32", "dev", device).CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to add IP alias %s to %s: %w (output: %s)", ip, device, err, string(out))
 	}
-
 	return nil
 }
 
 func (d *linuxBridgeDriver) RemoveIPAlias(ip, device string) error {
-	zap.L().Info("Removing IP alias from device", zap.String("ip", ip), zap.String("device", device))
-
-	// ip addr del <ip>/32 dev <device>
-	cmd := exec.Command("ip", "addr", "del", ip+"/32", "dev", device)
-	if out, err := cmd.CombinedOutput(); err != nil {
+	zap.L().Info("Removing IP alias on host ns", zap.String("ip", ip), zap.String("device", device))
+	if out, err := hostCmd("ip", "addr", "del", ip+"/32", "dev", device).CombinedOutput(); err != nil {
 		return fmt.Errorf("failed to remove IP alias %s from %s: %w (output: %s)", ip, device, err, string(out))
 	}
-
 	return nil
 }
